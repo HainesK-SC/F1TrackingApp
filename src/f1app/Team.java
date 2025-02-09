2092368e6fb6ba8fb0cc8b269e98b68bf619be1b
HainesK-SC <hainesk@sheridancollege.ca>
2025-02-09 12:25:51 -0500

    master

---
+++ src/f1app/Team.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package f1app;
+
+/**
+ *
+ * @author Kyle Haines - 2025
+ */
+public class Team {
+    private String teamName;
+    private String teamHome;
+    private Driver [] drivers = new Driver[2];
+    
+    public String getTeamName(){
+        return this.teamName;
+    }
+    
+    public String getTeamHome(){
+        return this.teamHome;
+    }
+    
+    public String driversOutput;
+    public String getDriver(){
+        
+        for(int i = 0; i < drivers.length; i++){
+          driversOutput += drivers[i] + "\n";  
+        }
+        return driversOutput;
+    }
+}
